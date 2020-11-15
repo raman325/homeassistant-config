0c2f0de8e10a7e9744968416bfe75752564cfaec
raman325 <7243222+raman325@users.noreply.github.com>
2020-11-15 03:15:39 -0500

    force bash script to fail if error occurs

--- update_configs_and_commit.sh
+++ update_configs_and_commit.sh
@@ -1,3 +1,19 @@
+# exit when any command fails
+set -e
+
+# Check for failed command and echo result
+check_exit() {
+  exit_code=$?
+  if [[ exit_code -ne 0  ]]; then
+    echo "\"${last_command}\" command failed with exit code $exit_code."
+  fi
+}
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting if needed
+trap 'check_exit' EXIT
+
 ./convert_configs_from_hjson_to_yaml.sh
 chmod -R 755 *
 git add .
