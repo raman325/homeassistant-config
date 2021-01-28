579c29871b2002f1dff56f482dab3dd391ccaaf1
raman325 <7243222+raman325@users.noreply.github.com>
2021-01-28 01:06:02 -0500

    change automation logic

--- pyscript/electric_blanket.py
+++ pyscript/electric_blanket.py
@@ -1,24 +1,29 @@
-from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, STATE_ON
+from datetime import timedelta
 
-SHERENE_BLANKET_PLUG = "switch.sherene_electric_blanket_plug"
+from homeassistant.util import dt
+
+SHERENE_BLANKET_PLUG = "sherene_electric_blanket_plug"
 
 
 @service
-def preheat_bed(entity_id: str = SHERENE_BLANKET_PLUG, minutes: float = 30):
+def preheat_bed(entity_name: str = SHERENE_BLANKET_PLUG, minutes: float = 30):
     """yaml
     description: Preheats Sherene's side of the bed by turning on the electric blanket temporarily
     fields:
-        entity_id:
-            description: Switch that controls the heat for the bed
-            example: switch.sherene_electric_blanket_plug
+        entity_name:
+            description: Optional entity name of switch and input_datetime that controls the heat for the bed and the time when it shuts off. Defaults to example.
+            example: sherene_electric_blanket_plug
         minutes:
             description: Number of minutes to run the electric blanket.
             example: 30
     """
 
-    if entity_id.split(".")[0] != "switch":
-        raise TypeError("Entity must be a switch.")
+    switch_entity = f"switch.{entity_name}"
+    input_dt_entity = f"input_datetime.{entity_name}"
 
-    switch.turn_on(entity_id=entity_id)
-    task.sleep(minutes * 60)
-    switch.turn_off(entity_id=entity_id)
+    if state.get(switch_entity) is None or state.get(input_dt_entity) is None:
+        raise TypeError("Entity name must exist as a switch and as an input_datetime.")
+
+    switch_off_time = dt.now() + timedelta(minutes=minutes)
+    switch.turn_on(entity_id=switch_entity)
+    input_datetime.set_datetime(entity_id=input_dt_entity, datetime=switch_off_time.strftime("%Y-%m-%d %H:%M:%S"))
