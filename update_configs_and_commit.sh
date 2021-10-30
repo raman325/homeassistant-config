05b3acd07985874c31824b29c16fca9077c357b1
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-10-30 10:29:55 -0400

    Add additional trusted proxy and remove hacs

--- update_configs_and_commit.sh
+++ update_configs_and_commit.sh
@@ -15,7 +15,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'check_exit' EXIT
 
 ./convert_configs_from_hjson_to_yaml.sh
-chmod -R 755 *
+#sudo chmod -R 755 *
 git add .
 git commit -m "$*"
 git push origin
